30d95c33fc0990b886702eed68215f59688864c0
Sergey Alexandrov <residx83@gmail.com>
2023-02-15 15:01:27 +0300

    Seminar_5 Done!

--- GB_Python/Seminar_5/Rec_Calc.py
+++ GB_Python/Seminar_5/Rec_Calc.py
@@ -9,4 +9,26 @@
 # на выходе:
 # 18
 
+def math_solver(solve: str):
+    if solve.isnumeric():
+        return int(solve)
+    else:
+        if "+" in solve:
+            temp = solve.split("+")
+            return math_solver(temp[0]) + math_solver(temp[1])
+        if "-" in solve:
+            temp = solve.split("-")
+            return math_solver(temp[0]) - math_solver(temp[1])
+        if "*" in solve:
+            temp = solve.split("*")
+            return math_solver(temp[0]) * math_solver(temp[1])
+        if "/" in solve:
+            temp = solve.split("/")
+            return math_solver(temp[0]) / math_solver(temp[1])
+
+
+string: str = input("Введите выражение: ")
+print(math_solver(string))
+print(eval(string))
+
 
